77c7543474a9e47fc482cde46092e253950cb502
Anh Nam <namanh272xh@gmail.com>
2026-04-10 15:43:08 +0700

    chore: init project structure

---
+++ config.py
@@ -0,0 +1,10 @@
+SYMBOLS = ["VCB.VN", "HPG.VN", "VIC.VN",
+           "MBB.VN", "TCB.VN"]  # banking/finance domain
+PERIOD  = "3mo"
+OUTPUT_DIR = "output"
+
+DB_CONFIG = {
+    "server"  : "localhost",
+    "database": "StockDB",
+    "driver"  : "ODBC Driver 17 for SQL Server"
+}
